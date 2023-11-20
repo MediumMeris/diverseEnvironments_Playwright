1433c64e987bbfdd2389a1bd6f62d7c12750e12d
MediumMeris <mediummeris@gmail.com>
2023-11-20 08:34:27 +0100

    Small change to globalSetup.ts

--- utils/globalSetup.ts
+++ utils/globalSetup.ts
@@ -5,13 +5,15 @@ import dotenv from "dotenv"
 
 async function globalSetup(config: FullConfig) {
 
-    if (process.env.test_env) {
-        dotenv.config({
-            path: `.env.${process.env.test_env}`,
-            override: true
-        })
-
+    try {
+        if (process.env.test_env) {
+            dotenv.config({
+                path: `.env.${process.env.test_env}`,
+                override: true
+            });
+        }
+    } catch (error) {
+        console.error("Error loading environment variables:", error);
     }
-    
 }
 export default globalSetup;
